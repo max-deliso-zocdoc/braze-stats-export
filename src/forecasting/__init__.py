453c25489e8e5b1f7a274dd5627d52b54e9440c5
Max DeLiso <max.deliso@zocdoc.com>
2025-06-25 11:13:55 -0400

    remove enhanced

--- src/forecasting/__init__.py
+++ src/forecasting/__init__.py
@@ -2,12 +2,10 @@
 
 from .linear_decay import StepBasedForecaster, QuietDatePredictor
 from .advanced_models import AdvancedForecaster, AdvancedForecastResult
-from .enhanced_predictor import EnhancedQuietDatePredictor
 
 __all__ = [
     "StepBasedForecaster",
     "QuietDatePredictor",
     "AdvancedForecaster",
     "AdvancedForecastResult",
-    "EnhancedQuietDatePredictor",
 ]
