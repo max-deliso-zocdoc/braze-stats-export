5313f758d4fddb6998b07193dcee00aabd9035a4
Max DeLiso <max.deliso@zocdoc.com>
2025-06-25 12:05:21 -0400

    remove inaccurate models

--- src/forecasting/__init__.py
+++ src/forecasting/__init__.py
@@ -1,11 +1,8 @@
 """Forecasting models for Canvas analytics."""
 
 from .linear_decay import StepBasedForecaster, QuietDatePredictor
-from .advanced_models import AdvancedForecaster, AdvancedForecastResult
 
 __all__ = [
     "StepBasedForecaster",
     "QuietDatePredictor",
-    "AdvancedForecaster",
-    "AdvancedForecastResult",
 ]
